10fa6d1a2f055d611d74f1d76c768fbd780182b8
iorp <104486888+iorp@users.noreply.github.com>
2023-10-15 01:08:56 -0700

    0

--- js/proto.js
+++ js/proto.js
@@ -32,61 +32,74 @@ Object.prototype.merge = function (obj) {
       { ...this }
     );
   };
-
  
-
-function loadResources(resources, callback) {  
-     // Example usage with an array of resources
-//   const resourceArray = [
-//     'your-css-file.css',
-//     'your-js-file.js',
-//     'your-other-css-file.cssString { background-color: yellow; }'
-//   ];
+  async function include(resources, callback) {
+/*   
+    Example usage with an array of resources and callback
+  const resourceArray = [
+    'your-css-file.css',
+    'your-js-file.js',
+    'your-other-css-file.cssString { background-color: yellow; }',
+  ];
   
-//   loadResources(resourceArray, () => {
-//     // Your callback code here
-//   });
+  loadResources(resourceArray, () => {
+    console.log('All resources have been loaded.');
+  });
   
+  // Example usage with an array of resources and async/await
+  (async () => {
+    console.log('Loading resources...');
+    await loadResources(resourceArray);
+    console.log('All resources have been loaded.');
+  })();
+*/
     if (!Array.isArray(resources) || resources.length === 0) {
       return;
     }
   
-    const loadResource = (resource, index) => {
-      if (resource.endsWith('.css')) {
-        if (!document.querySelector(`link[href="${resource}"]`)) {
-          const link = document.createElement('link');
-          link.rel = 'stylesheet';
-          link.type = 'text/css';
-          link.href = resource;
-          document.head.appendChild(link);
-        }
-      } else if (resource.endsWith('.js')) {
-        if (!document.querySelector(`script[src="${resource}"]`)) {
-          const script = document.createElement('script');
-          script.src = resource;
-          if (index === resources.length - 1 && callback) {
-            script.onload = callback;
+    const loadResource = (resource) => {
+      return new Promise((resolve) => {
+        if (resource.endsWith('.css')) {
+          if (!document.querySelector(`link[href="${resource}"]`)) {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = resource;
+            document.head.appendChild(link);
           }
-          document.head.appendChild(script);
+        } else if (resource.endsWith('.js')) {
+          if (!document.querySelector(`script[src="${resource}"]`)) {
+            const script = document.createElement('script');
+            script.src = resource;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          }
+        } else if (resource.endsWith('.cssString')) {
+          const styleTag = document.querySelector(`style[data-custom-style="${resource}"]`);
+          if (!styleTag) {
+            const style = document.createElement('style');
+            style.setAttribute('data-custom-style', resource);
+            style.textContent = resource;
+            document.head.appendChild(style);
+          }
+          resolve();
         }
-      } else if (resource.endsWith('.cssString')) {
-        const styleTag = document.querySelector(`style[data-custom-style="${resource}"]`);
-        if (!styleTag) {
-          const style = document.createElement('style');
-          style.setAttribute('data-custom-style', resource);
-          style.textContent = resource;
-          document.head.appendChild(style);
-        }
-      }
-  
-      if (index < resources.length - 1) {
-        loadResource(resources[index + 1], index + 1);
-      }
+      });
     };
   
-    loadResource(resources[0], 0);
+    if (callback) {
+      const promises = resources.map((resource) => loadResource(resource));
+      Promise.all(promises).then(() => {
+        callback();
+      });
+    } else {
+      for (const resource of resources) {
+        await loadResource(resource);
+      }
+    }
   }
   
+
 
 
 
